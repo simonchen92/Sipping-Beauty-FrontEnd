c567c8ba83fe659c70fe52a333c80bfe8de5eacc
simonchen92 <simon.k.chen92@gmail.com>
2020-01-08 15:18:05 -0500

    Centered text on home page / landing page

--- src/components/HomePage/home.js
+++ src/components/HomePage/home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const HomePage = styled.div`
+  text-align: center;
+  margin: 1em;
+`
 
 const Home = () => (
-  <div className="home-page">
-    <h2>Welcome!</h2>
-    <p>This is a beer application to search for information about your favorite beer</p>
-  </div>
+  <HomePage>
+    <div className="home-page">
+      <h2>Welcome!</h2>
+      <p>This is a beer application to search for information about your favorite beer</p>
+    </div>
+  </HomePage>
 )
 
 export default Home
