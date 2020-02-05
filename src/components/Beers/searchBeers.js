3f1eaf76e1e21b33f517098a5dd208bccdacf064
simonchen92 <simon.k.chen92@gmail.com>
2020-02-05 17:42:17 -0500

    Got rid of unused routes

--- src/components/Beers/searchBeers.js
+++ src/components/Beers/searchBeers.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-// import { Redirect } from 'react-router'
 
 import { searchBeers, addBeer } from '../../api/beer'
 import messages from '../AutoDismissAlert/messages'
