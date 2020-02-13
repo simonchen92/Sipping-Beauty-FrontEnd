b11d3feda33446a6b04d9a01cd45768b6a745877
simonchen92 <simon.k.chen92@gmail.com>
2020-02-13 17:06:37 -0500

    Changed border setting to white

--- src/styling/SearchBeerWrapper.js
+++ src/styling/SearchBeerWrapper.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const SearchBeerWrapper = styled.div`
-    border: 1px solid black;
+    border: 1px solid white;
     background-color: white;
     border-radius: 8px;
     box-shadow: 5px 6px 8px 0px rgba(153,153,153,0.5);
