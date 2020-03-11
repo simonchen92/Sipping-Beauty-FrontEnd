f92b09021cd99e4716606f327a9f9902f7029af5
simonchen92 <simon.k.chen92@gmail.com>
2020-03-11 14:24:55 -0400

    Created a header for create beer section
    Create header for beer creation
    Added styling to create beer header

---
+++ src/styling/CreateBeerHeader.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const CreateBeerHeader = styled.h3`
+    color: white;
+    font-family: 'Gloria Hallelujah', cursive;
+    text-align: center;
+    margin: 1em;
+`
